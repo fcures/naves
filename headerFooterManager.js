ab1b729b2e89cb7baf9463287fce73a3c05959c8
sevan <140277466+fcures@users.noreply.github.com>
2024-06-04 22:49:42 -0700

    Update headerFooterManager.js

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -1,6 +1,12 @@
 class SpecialHeader extends HTMLElement {
   connectedCallback(){
     this.innerHTML = `
+
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon" href="./favi.ico">
+    <link rel="stylesheet" href="/styles.css">
+    <title>Sevan's Portfolio</title>
+    
     <button class="tablink"><a href = "/">Home</a></button>
     <button class="tablink"><a href = "/about">About Me</a></button>
   `
