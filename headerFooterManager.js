03ee6d477bbdedf44e4abe887ab4a5c9cb05bbaf
sevan <140277466+fcures@users.noreply.github.com>
2024-06-05 01:09:35 -0700

    Update headerFooterManager.js

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -84,7 +84,7 @@ li.dropdown {
     </div>
   </li>
   
-  <li>style="float:right"><a class="active" href="/about">About</a></li>
+  <li style="float:right"><a class="active" href="/about">About</a></li>
 </ul>
   `
   }
