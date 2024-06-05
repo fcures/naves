e2e23065a99e39f47ea387e6f3a066d5f54c3991
sevan <140277466+fcures@users.noreply.github.com>
2024-06-04 22:44:19 -0700

    Update headerFooterManager.js

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -1,1 +1,21 @@
+class SpecialHeader extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+    <button class="tablink"><a href = "/">Home</a></button>
+    <button class="tablink"><a href = "/about">About Me</a></button>
+  `
+  }
+}
 
+class SpecialFooter extends HTMLElement {
+  connectedCallback(){
+    this.innerHTML = `
+    <p>
+      Naves.dev
+    </p>
+  `
+  }
+}
+
+customElements.define('special-header', SpecialHeader)
+customElements.define('special-footer', SpecialFooter)
