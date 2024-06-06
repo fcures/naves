4aecab96b24318a10864611fc7386192c47bba1e
sevan <140277466+fcures@users.noreply.github.com>
2024-06-06 10:48:12 -0700

    Update postsManager.js

--- postsManager.js
+++ postsManager.js
@@ -1,1 +1,11 @@
+class SpecialPosts extends HTMLElement {
+  connectedCallback(){
 
+    var fs = require('fs');
+    var files = fs.readdirSync('/posts/');
+
+    this.innerHTML = files
+  }
+}
+
+customElements.define('special-posts', SpecialPostsr)
