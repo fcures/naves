aa6d8fccb6ab4324694d255af56e42d47eae908e
sevan <140277466+fcures@users.noreply.github.com>
2024-06-07 18:56:13 -0700

    Update postsManager.js

--- postsManager.js
+++ postsManager.js
@@ -1,10 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 class SpecialPosts extends HTMLElement {
   connectedCallback(){
 
     var fs = require('fs');
     var files = fs.readdirSync('/posts/');
 
-    this.innerHTML = files[1]
+    this.innerHTML = const __filename = fileURLToPath(import.meta.url);
   }
 }
 
